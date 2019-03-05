14e46fe218bac46b66acdd35ab38b317c6ead13b
Joseph Anderson <josepha93@gmail.com>
2019-03-05 11:18:06 -0500

    Cleanup

--- Yabber/Formats/GPARAM.cs
+++ Yabber/Formats/GPARAM.cs
@@ -1,77 +1,54 @@
 ﻿using SoulsFormats;
 using System;
 using System.IO;
-using System.Xml;
 using System.Numerics;
+using System.Xml;
 
 namespace Yabber
 {
     static class YGPARAM
     {
-        private static string TypeToString(GPARAM.ParamType type)
-        {
-            switch (type)
-            {
-                case GPARAM.ParamType.Unk1:
-                    return "byte";
-                case GPARAM.ParamType.Unk2:
-                    return "short";
-                case GPARAM.ParamType.Unk3:
-                    return "int";
-                case GPARAM.ParamType.Unk5:
-                    return "bool";
-                case GPARAM.ParamType.Unk7:
-                    return "int2";
-                case GPARAM.ParamType.Unk9:
-                    return "float";
-                case GPARAM.ParamType.UnkB:
-                    return "bool";
-                case GPARAM.ParamType.UnkC:
-                    return "fvec2";
-                case GPARAM.ParamType.UnkE:
-                    return "fvec4";
-                case GPARAM.ParamType.UnkF:
-                    return "bytes";
-            }
-            return null;
-        }
-
         public static void Unpack(this GPARAM gparam, string sourceFile)
         {
             XmlWriterSettings xws = new XmlWriterSettings();
             xws.Indent = true;
-            xws.IndentChars = "    ";
             XmlWriter xw = XmlWriter.Create($"{sourceFile}.xml", xws);
+
             xw.WriteStartElement("gparam");
             xw.WriteElementString("unk1", gparam.Unk1.ToString());
             xw.WriteStartElement("groups");
-            foreach (var group in gparam.Groups)
+            foreach (GPARAM.Group group in gparam.Groups)
             {
                 xw.WriteStartElement("group");
                 xw.WriteAttributeString("name1", group.Name1);
                 xw.WriteAttributeString("name2", group.Name2);
-                for (var p = 0; p < group.Params.Count; p++)
+                foreach (GPARAM.Param param in group.Params)
                 {
-                    var param = group.Params[p];
-                    xw.WriteStartElement("params");
+                    xw.WriteStartElement("param");
                     xw.WriteAttributeString("name1", param.Name1);
                     xw.WriteAttributeString("name2", param.Name2);
-                    xw.WriteAttributeString("type", TypeToString(param.Type));
+                    xw.WriteAttributeString("type", param.Type.ToString());
                     for (var i = 0; i < param.Values.Count; i++)
                     {
                         xw.WriteStartElement("value");
                         xw.WriteAttributeString("id", param.Unk1Values[i].ToString());
                         if (i < group.Comments.Count)
                             xw.WriteAttributeString("comment", group.Comments[i]);
-                        if (param.Values[i] is Vector2)
+
+                        if (param.Type == GPARAM.ParamType.Float2)
                         {
                             var value = (Vector2)param.Values[i];
-                            xw.WriteString($@"{value.X} {value.Y}");
+                            xw.WriteString($"{value.X}, {value.Y}");
                         }
-                        else if (param.Values[i] is Vector4)
+                        else if (param.Type == GPARAM.ParamType.Float4)
                         {
                             var value = (Vector4)param.Values[i];
-                            xw.WriteString($@"{value.X} {value.Y} {value.Z} {value.W}");
+                            xw.WriteString($"{value.X}, {value.Y}, {value.Z}, {value.W}");
+                        }
+                        else if (param.Type == GPARAM.ParamType.Byte4)
+                        {
+                            var value = (byte[])param.Values[i];
+                            xw.WriteString($"{value[0]:X2} {value[1]:X2} {value[2]:X2} {value[3]:X2}");
                         }
                         else
                         {
@@ -98,7 +75,7 @@ namespace Yabber
             }
             xw.WriteEndElement();
 
-            xw.WriteElementString("unkBlock", string.Join(" ", gparam.UnkBlock2));
+            xw.WriteElementString("unkBlock", Convert.ToBase64String(gparam.UnkBlock2));
 
             xw.WriteEndElement();
             xw.Close();
